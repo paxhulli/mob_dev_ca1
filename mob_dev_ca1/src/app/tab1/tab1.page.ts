385893e64fe6cbf887122391ea96d3f05e8eccbd
paxhulli <paxhulli@hotmail.com>
2023-05-08 21:02:41 +0000

    tab 2 and 3

--- mob_dev_ca1/src/app/tab1/tab1.page.ts
+++ mob_dev_ca1/src/app/tab1/tab1.page.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
-
+  myVariable : String = "Hello";
+   
+    updateMyValue(){
+        this.myVariable = "Welcome to our page";}
 }
