dfeedef47423389af405d9c5361b1bfa95e31d3a
paxhulli <paxhulli@hotmail.com>
2023-05-08 20:30:35 +0000

    home

---
+++ mob_dev_ca1/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mob_dev_ca1',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
